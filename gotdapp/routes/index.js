06eab07906cd6260d456b94f7ac4ba310f945f51
PhilipMathieu <philip.eng.mathieu@gmail.com>
2017-03-25 13:27:30 -0400

    added basic app pages

--- gotdapp/routes/index.js
+++ gotdapp/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Game of the Day' });
+});
+
+router.get('/games', function(req, res, next) {
+  res.render('games', { title: 'Today\'s Games' });
+});
+
+router.post('/games', function(req, res) {
+    var name = req.body.name;
+        roster = req.body.roster;
+    res.render('games', { title: 'Today\'s Games' });
 });
 
 module.exports = router;
