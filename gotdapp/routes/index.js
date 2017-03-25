13b90c12cabfbe6b4b76dd90cf263999091858d1
PhilipMathieu <philip.eng.mathieu@gmail.com>
2017-03-25 15:06:21 -0400

    Got app to work kinda

--- gotdapp/routes/index.js
+++ gotdapp/routes/index.js
@@ -1,5 +1,42 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router()
+
+function getGames() {
+    return [{"home_team_abbrev":"CWS","away_team_abbrev":"BOS","time":"7:05 PM"}, {"home_team_abbrev":"PEM","away_team_abbrev":"NEM","time":"7:05 PM"}];
+}
+
+function parsePlayers(roster) {
+    pattern = new RegExp(/[A-Z]{1}[a-z\.]{1}[A-z\.]+ [A-Z]{1}[a-z\.]{1}[A-z\.]+, [A-z]{3}/g);
+    var players = roster.match(pattern);
+    /* Remove breaking news */
+    for (var i=players.length-1; i>=0; i--) {
+        if (players[i] === "Breaking News") {
+            players.splice(i, 1);
+        }
+        if (players[i] === "Recent News") {
+            players.splice(i, 1);
+        }
+    }
+    return players;
+};
+
+function scoreGames(players) {
+    var games = getGames();
+    var scored = [];
+    for (var i=0; i<games.length; i++) {
+        var curr = {"game":games[i],"score":0};
+        for (var j=0; j<players.length; j++) {
+            if(games[i]["home_team_abbrev"] == players[j].substr(players[j].length-3).toUpperCase()) {
+                curr["score"] += 1;
+            }
+            if(games[i]["away_team_abbrev"] == players[j].substr(players[j].length-3).toUpperCase()) {
+                curr["score"] += 1;
+            }
+        }
+        scored.push(curr);
+    }
+    return scored.sort(function compscore(a, b) {return b["score"] - a["score"];});
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,13 +44,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/games', function(req, res, next) {
-  res.render('games', { title: 'Today\'s Games' });
+  res.render('games', { title: 'Today\'s Games', home: "h", away: "a", score: 0 });
 });
 
 router.post('/games', function(req, res) {
     var name = req.body.name;
         roster = req.body.roster;
-    res.render('games', { title: 'Today\'s Games' });
+    var players = parsePlayers(roster);
+    var games = scoreGames(players);
+    res.render('games', { title: 'Today\'s Games', home: games[0]["game"]["home_team_abbrev"], away: games[0]["game"]["away_team_abbrev"], score: games[0]["score"], time: games[0]["game"]["time"]});
 });
 
 module.exports = router;
